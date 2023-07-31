d9c633bd08982430ce4f3750a1a2c2d24d5bb6cb
Ixyz.dev <kevin.diaz@alumnos.uv.cl>
2023-07-30 22:46:16 -0400

    A

--- app/handDetector/handDetectorClass.py
+++ app/handDetector/handDetectorClass.py
@@ -1,4 +1,3 @@
-# Importamos las librerías necesarias
 import cv2
 import mediapipe as mp
 
@@ -8,20 +7,13 @@ from .controllers.findPosition import findPosition
 from .controllers.fingersUp import fingersUp
 from .controllers.findDistance import findDistance
 
-# Creamos la clase handDetector
-
 
 class handDetector():
-    # Inicializamos la clase con los parámetros deseados
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
-        # mode: en este modo, si el número máximo de manos ya ha sido alcanzado, simplemente detecta la presencia de manos adicionales, pero no rastrea su movimiento.
-        self.mode = mode
-        # maxHands: número máximo de manos a detectar
-        self.maxHands = maxHands
-        # detectionCon: umbral de confianza para la detección de la mano
-        self.detectionCon = detectionCon
-        # trackCon: umbral de confianza para el seguimiento de la mano
-        self.trackCon = trackCon
+        self.mode = mode  # detectar si el número máximo de manos ya ha sido alcanzado
+        self.maxHands = maxHands  # máximo de manos a detectar
+        self.detectionCon = detectionCon  # confianza para la detección de la mano
+        self.trackCon = trackCon  # confianza para el seguimiento de la mano
 
         # Cargamos la solución de manos de MediaPipe y configuramos con los parámetros definidos
         self.mpHands = mp.solutions.hands
